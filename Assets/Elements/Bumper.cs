d75ffa4fff6621cb4848eb625fe13461d1250a44
Victor Fanton <victorp.fanton@gmail.com>
2021-11-23 15:27:27 +0100

    Le bumper fait de la lumière quand il est bumpé
    Signed-off-by: evann362 <60409453+evann362@users.noreply.github.com>

--- Assets/Elements/Bumper.cs
+++ Assets/Elements/Bumper.cs
@@ -5,11 +5,31 @@ using UnityEngine;
 public class Bumper : MonoBehaviour
 {
     [SerializeField] private float strength = 4f;
+    [SerializeField] private GameObject bumpLights;
+    [SerializeField] private float bumpLightsDuration = .2f;
 
-    private void OnCollisionEnter(Collision collision)
+	private void Start()
+	{
+        DeactivateLights();
+	}
+
+	private void OnCollisionEnter(Collision collision)
     {
         Vector3 direction = collision.gameObject.transform.position - transform.position;
         collision.rigidbody.AddForce(direction.normalized * strength, ForceMode.Impulse);
         Debug.Log("BUMP !");
+
+        BumpFX();
     }
+
+    private void BumpFX()
+	{
+        bumpLights.SetActive(true);
+        Invoke("DeactivateLights", bumpLightsDuration);
+	}
+
+    private void DeactivateLights()
+	{
+        bumpLights.SetActive(false);
+	}
 }
