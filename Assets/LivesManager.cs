7cdf8c24e34310ccd9b58c697135f02ee0d0edb2
Victor Fanton <jiltofar@gmail.com>
2021-11-27 18:47:47 +0100

    Vies réparées

--- Assets/LivesManager.cs
+++ Assets/LivesManager.cs
@@ -7,13 +7,13 @@ using UnityEngine.UI;
 public class LivesManager : MonoBehaviour
 {
     [SerializeField] private int maxLives;
-    [SerializeField] private int currentLives;
+    private int currentLives;
 
     [SerializeField] private Text livesUIText;
 
     private PointsManager pointsManager;
 
-    private void Awake()
+    private void Start()
     {
         currentLives = maxLives;
         livesUIText.text = currentLives + " Lives";
